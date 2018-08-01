8e3e02dc1702847dea8eae871ff5cde69007cc24
Emmanuel Amponsah <emampons@gmail.com>
2018-07-31 20:27:08 -0500

    additional test

--- tests/bigint_test.cpp
+++ tests/bigint_test.cpp
@@ -29,17 +29,23 @@ TEST_F(BigIntTest,createBigIntString){
 }
 
 TEST_F(BigIntTest,addBigIntLongs){
+    num1 = BigInt(56);
     num1 = num1 + num1;
     ASSERT_EQ("112", num1.to_string());
+
 }
 
 TEST_F(BigIntTest,addBigIntStrings){
+    num2 = BigInt("45");
     num2 = num2 + num2;
-    ASSERT_EQ("112", num2.to_string());
+    ASSERT_EQ("90", num2.to_string());
 }
 
 TEST_F(BigIntTest,addDifferent){
+    num1 = BigInt(56);
+    num2 = BigInt("45");
     num1 = num1 + num2;
-    ASSERT_EQ("112", num1.to_string());
+    ASSERT_EQ("101", num1.to_string());
+
 }
 
